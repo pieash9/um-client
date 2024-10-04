371314f2abf7d36a1d188ff86c05f91955e47885
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-10-04 23:29:50 +0600

    added login fn

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,37 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [login, { data, error }] = useLoginMutation();
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      id: "0001",
+      password: "admin12345",
+    },
+  });
+
+  console.log({ data, error });
+
+  const onSubmit = (data) => {
+    const userInfo = { id: data.id, password: data.password };
+    login(userInfo);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="">
+        <label htmlFor="id">ID:</label>
+        <input type="text" id="id" {...register("id")} />
+      </div>
+      <div className="">
+        <label htmlFor="password">Password:</label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button type="primary" htmlType="submit">
+        Login
+      </Button>
+    </form>
+  );
 };
 
 export default Login;

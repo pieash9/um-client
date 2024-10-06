76d8ad7639d9ce33ddff1c006283b9aecd8cf853
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-10-06 08:00:35 +0600

    added protected route

---
+++ src/redux/features/academicSemester/academicSemesterApi.ts
@@ -0,0 +1,11 @@
+import { baseApi } from "../../api/baseApi";
+
+const academicSemesterApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllSemesters: builder.query({
+      query: () => ({
+        url: "",
+      }),
+    }),
+  }),
+});

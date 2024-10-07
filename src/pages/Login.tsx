6044226aa4467fbd2da18561553a98955237280d
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-10-07 08:08:03 +0600

    update refresh token handled

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,6 +6,7 @@ import { setUser, TUser } from "../redux/features/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import UFrom from "../components/form/UFrom";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -19,22 +20,22 @@ const Login = () => {
   });
 
   const onSubmit = async (data: FieldValues) => {
-    const toastId = toast.loading("Logging in...");
-    try {
-      const userInfo = { id: data.id, password: data.password };
-      const res = await login(userInfo).unwrap();
-
-      const user = verifyToken(res.data.accessToken) as TUser;
-      dispatch(setUser({ user: user, token: res.data.accessToken }));
-      toast.success("Login successful", { id: toastId, duration: 2000 });
-      navigate(`/${user.role}/dashboard`);
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    } catch (err) {
-      toast.error("Something went wrong", { id: toastId, duration: 2000 });
-    }
+    console.log(data);
+    // const toastId = toast.loading("Logging in...");
+    // try {
+    //   const userInfo = { id: data.id, password: data.password };
+    //   const res = await login(userInfo).unwrap();
+    //   const user = verifyToken(res.data.accessToken) as TUser;
+    //   dispatch(setUser({ user: user, token: res.data.accessToken }));
+    //   toast.success("Login successful", { id: toastId, duration: 2000 });
+    //   navigate(`/${user.role}/dashboard`);
+    //   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    // } catch (err) {
+    //   toast.error("Something went wrong", { id: toastId, duration: 2000 });
+    // }
   };
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <UFrom onSubmit={handleSubmit(onSubmit)}>
       <div className="">
         <label htmlFor="id">ID:</label>
         <input type="text" id="id" {...register("id")} />
@@ -46,7 +47,7 @@ const Login = () => {
       <Button type="primary" htmlType="submit">
         Login
       </Button>
-    </form>
+    </UFrom>
   );
 };
 
--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,5 +1,13 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  BaseQueryApi,
+  BaseQueryFn,
+  createApi,
+  DefinitionType,
+  FetchArgs,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
+import { logout, setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "https://um-server-bay.vercel.app/api/v1",
@@ -15,9 +23,36 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
-const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
-  const result = await baseQuery(args, api, extraOptions);
-  console.log(result);
+const baseQueryWithRefreshToken: BaseQueryFn<
+  FetchArgs,
+  BaseQueryApi,
+  DefinitionType
+> = async (args, api, extraOptions): Promise<any> => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 401) {
+    const res = await fetch(
+      `https://um-server-bay.vercel.app/api/v1/auth/refresh-token`,
+      {
+        method: "POST",
+        credentials: "include",
+      }
+    );
+
+    const data = await res.json();
+
+    if (data?.data?.accessToken) {
+      const user = (api.getState() as RootState).auth.user;
+
+      api.dispatch(setUser({ user: user, token: data.data.accessToken }));
+
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logout());
+    }
+  }
+
+  return result;
 };
 
 export const baseApi = createApi({
